f447d5d6001baa89a41033a803e5b2542696c87e
MASAYA-math <68306375+MASAYA-math@users.noreply.github.com>
2020-12-25 19:03:24 +0900

    Change a file "maerd.py"

--- maerd.py
+++ maerd.py
@@ -3,14 +3,27 @@ import pyxel as px
 
 class App:
     def __init__(self):
-        px.init(256, 256)
+        px.init(256, 256, caption="MAERD")
+        px.load("resource.pyxres")
+        self.x = 112
+        self.y = 128
         px.run(self.update, self.draw)
 
     def update(self):
-        pass
+        if px.btn(px.KEY_D):
+            self.x += 1
+        if px.btn(px.KEY_A):
+            self.x += -1
+        if px.btn(px.KEY_W):
+            self.y += -1
+        if px.btn(px.KEY_S):
+            self.y += 1
 
     def draw(self):
-        pass
+        px.cls(0)
+        px.bltm(0, 0, 0, 0, 0, 32, 32)
+        px.blt(self.x, self.y, 0, 0, 48, 16, 16, 0)
+        px.blt(self.x + 16, self.y, 0, 0, 64, 16, 16, 0)
 
 
 App()
