32670e349ab9b8cfb1c5df21e8dfa281f7ba60ea
MASAYA-math <68306375+MASAYA-math@users.noreply.github.com>
2020-12-26 17:40:45 +0900

    Refactor some files

--- engine/main.py
+++ engine/main.py
@@ -12,3 +12,17 @@ class CollisionBlock(Block):
 
 class EventBlock(Block):
     pass
+
+
+def make_map_instance(source):
+    map_data_instance = []
+    for i in range(0, len(source)):
+        map_data_instance.append([])
+        for j in range(0, len(source[i])):
+            if source[i][j] == 0:
+                map_data_instance[i].append(EmptyBlock())
+            elif source[i][j] == 1:
+                map_data_instance[i].append(CollisionBlock())
+            elif source[i][j] == 2:
+                map_data_instance[i].append(EventBlock())
+    return map_data_instance
--- map0.py
+++ map0.py
@@ -1,18 +1,11 @@
 from engine import main
 
+
 map_data_source = [
     [0, 1, 0]
 ]
 
-map_data_instance = []
-for i in range(0, len(map_data_source)):
-    map_data_instance.append([])
-    for j in range(0, len(map_data_source[i])):
-        if map_data_source[i][j] == 0:
-            map_data_instance[i].append(main.EmptyBlock())
-        elif map_data_source[i][j] == 1:
-            map_data_instance[i].append(main.CollisionBlock())
-        elif map_data_source[i][j] == 2:
-            map_data_instance[i].append(main.EventBlock())
+map_data_instance = main.make_map_instance(map_data_source)
+
 
 print(map_data_instance)
