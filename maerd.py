6f9cb8ad918d6d0394f10063939a7f09d19c2d49
MASAYA-math <68306375+MASAYA-math@users.noreply.github.com>
2020-12-26 14:00:05 +0900

    Change some files.

--- maerd.py
+++ maerd.py
@@ -1,6 +1,9 @@
 import pyxel as px
 
 
+col_blocks = [[24, 0]]
+
+
 class Player:
     def __init__(self, x, y):
         self.x, self.y = x, y
@@ -19,6 +22,15 @@ class Player:
         px.blt(self.x, self.y, 0, 0, 48, 16, 16, 0)
 
 
+def is_col_flagged(player: Player) -> bool:
+    for i in range(0, len(col_blocks)):
+        if (abs(player.x - col_blocks[i][0]) <= 16 and
+                abs(player.y - col_blocks[i][1]) <= 16):
+            return True
+        else:
+            return False
+
+
 class App:
     def __init__(self):
         px.init(256, 256, caption="MAERD")
@@ -29,10 +41,10 @@ class App:
 
     def update(self):
         self.player.update()
-        if self.player.y < 16:
-            self.map = 1
-            self.player.x = 112
-            self.player.y = 128
+        # if self.player.y < 16:
+        #     self.map = 1
+        #     self.player.x = 112
+        #     self.player.y = 128
 
     def draw(self):
         px.cls(0)
@@ -40,6 +52,8 @@ class App:
             px.bltm(0, 0, 0, 0, 0, 32, 32)
             self.player.draw()
             px.blt(self.player.x + 16, self.player.y, 0, 0, 64, 16, 16, 0)
+            if is_col_flagged(self.player):
+                px.text(0, 0, "HIT", 8)
         elif self.map == 1:
             self.player.draw()
 
