4866ad6246dfbde757cdc417be766cb5a32be6b3
MASAYA-math <68306375+MASAYA-math@users.noreply.github.com>
2020-12-26 15:23:37 +0900

    Change a file "maerd.py"

--- maerd.py
+++ maerd.py
@@ -9,7 +9,7 @@ class Player:
         self.x, self.y = x, y
 
     def update(self):
-        _, forbidden_direction = is_col_flagged(self)
+        forbidden_direction = is_col_flagged(self)
         print(forbidden_direction)
         if px.btn(px.KEY_D) and not forbidden_direction["D"]:
             self.x += 1
@@ -24,7 +24,7 @@ class Player:
         px.blt(self.x, self.y, 0, 0, 48, 16, 16, 0)
 
 
-def is_col_flagged(player: Player) -> tuple:
+def is_col_flagged(player: Player) -> dict:
     forbidden_direction = {"A": False, "D": False, "W": False, "S": False}
     for i in range(0, len(col_blocks)):
         if (abs(player.x - col_blocks[i][0]) <= 16 and
@@ -41,9 +41,9 @@ def is_col_flagged(player: Player) -> tuple:
             if player.y - col_blocks[i][1] + -16 < -16 and\
                     abs(player.x - col_blocks[i][0]) != 16:
                 forbidden_direction["S"] = True
-            return True, forbidden_direction
+            return forbidden_direction
         else:
-            return False, forbidden_direction
+            return forbidden_direction
 
 
 class App:
@@ -56,10 +56,6 @@ class App:
 
     def update(self):
         self.player.update()
-        # if self.player.y < 16:
-        #     self.map = 1
-        #     self.player.x = 112
-        #     self.player.y = 128
 
     def draw(self):
         px.cls(0)
@@ -67,8 +63,6 @@ class App:
             px.bltm(0, 0, 0, 0, 0, 32, 32)
             self.player.draw()
             px.blt(self.player.x + 16, self.player.y, 0, 0, 64, 16, 16, 0)
-            if is_col_flagged(self.player):
-                px.text(0, 0, "HIT", 8)
         elif self.map == 1:
             self.player.draw()
 
