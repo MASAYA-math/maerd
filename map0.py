9a24e275a0c4aa9d275b9857936f375f8b65076c
MASAYA-math <68306375+MASAYA-math@users.noreply.github.com>
2020-12-28 17:11:52 +0900

    Refactor a file "map0.py"

--- map0.py
+++ map0.py
@@ -13,9 +13,9 @@ map_data_source = [
     [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
 ]
 
-map_data_instance = engine.make_map_instance(map_data_source, events.events)
+map_data = engine.make_map_instance(map_data_source, events.events)
 
 
 class Map:
     def __init__(self):
-        self.map_data = map_data_instance
+        self.map_data = map_data
