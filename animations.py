81ffac583e88b6158179744e49f3a38bf7173551
MASAYA-math <68306375+MASAYA-math@users.noreply.github.com>
2021-01-12 15:15:03 +0900

    Change some files

--- animations.py
+++ animations.py
@@ -2,7 +2,7 @@ import pyxel as px
 from engine import engine
 
 
-class Animation1(engine.Animation):
+class Animation0(engine.Animation):
     def __init__(self):
         super().__init__()
         self.is_playing = True
@@ -12,6 +12,35 @@ class Animation1(engine.Animation):
         if self.is_playing:
             self.tick_count += 1
         if self.tick_count >= 60:
+            self.is_playing = False
+            self.tick_count = 0
+
+    def draw(self):
+        super().draw()
+        if self.is_playing:
+            px.cls(0)
+            if self.tick_count < 12:
+                px.text(0, 0, "M", 8)
+            elif self.tick_count < 24:
+                px.text(0, 0, "MA", 8)
+            elif self.tick_count < 36:
+                px.text(0, 0, "MAE", 8)
+            elif self.tick_count < 48:
+                px.text(0, 0, "MAER", 8)
+            else:
+                px.text(0, 0, "MAERD", 8)
+
+
+class Animation1(engine.Animation):
+    def __init__(self):
+        super().__init__()
+        self.is_playing = False
+
+    def update(self):
+        super().update()
+        if self.is_playing:
+            self.tick_count += 1
+        if self.tick_count >= 30:
             self.is_playing = False
             self.tick_count = 0
 
--- maerd.py
+++ maerd.py
@@ -8,7 +8,7 @@ TITLE = "MAERD"
 RESOURCE_FILE = "../assets/resource.pyxres"
 PLAYER = player.Player(112, 128)
 MAPS = [map0.Map0, map1.Map1]
-ANIMATIONS = [animations.Animation1()]
+ANIMATIONS = [animations.Animation0(), animations.Animation1()]
 
 
 engine.App(TITLE, RESOURCE_FILE, PLAYER, MAPS, ANIMATIONS)
